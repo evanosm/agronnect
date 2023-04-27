370e67bfad528dd9e2e6498a999ad2c04b98ddd5
Evan OSMONT <evan.osmont@gmail.com>
2023-04-27 09:31:21 +0200

    Projects Files, Site controller init, CSS modifications

--- Controllers/ServicesController.cs
+++ Controllers/ServicesController.cs
@@ -22,14 +22,57 @@ public class ServicesController : Controller
     }
 
     [HttpPost]
-    public async Task<IActionResult> Index(string name)
+    public async Task<IActionResult> Add(string Name)
     {
         var newService = new Service()
         {
-            Name = name
+            Name = Name
         };
         _context.Services.Add(newService);
         await _context.SaveChangesAsync();
         return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(int Id, string Name)
+    {
+        var service = await _context.Services.FindAsync(Id);
+        if (service == null)
+        {
+            return NotFound();
+        }
+
+        service.Name = Name;
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int Id)
+    {
+        var service = await _context.Services.FindAsync(Id);
+        if (service == null)
+        {
+            return NotFound();
+        }
+
+        _context.Services.Remove(service);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(int Id)
+    {
+        var service = await _context.Services
+            .Include(s => s.Employees)
+            .ThenInclude(e => e.Site)
+            .FirstOrDefaultAsync(s => s.ServiceId == Id);
+        if (service == null)
+        {
+            return NotFound();
+        }
+
+        return View(service);
     }
 }
