bbab4f64c54cfc15450236f108e6d57e173e0335
Evan OSMONT <evan.osmont@gmail.com>
2023-04-27 11:31:06 +0200

    Site Controller and Views

--- Controllers/SitesController.cs
+++ Controllers/SitesController.cs
@@ -1,4 +1,5 @@
 using AnnuaireCESI.Data;
+using AnnuaireCESI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,7 +16,65 @@ public class SitesController : Controller
 
     public async Task<IActionResult> Index()
     {
-        var sites = _context.Sites.Include(s => s.Employees).ToListAsync();
-        return View(await sites);
+        var sites = await _context.Sites.Include(s => s.Employees).ToListAsync();
+        return View(sites);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(string City, string Description)
+    {
+        var newSite = new Site()
+        {
+            City = City,
+            Description = Description
+        };
+        _context.Sites.Add(newSite);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(int Id, string City, string Description)
+    {
+        Console.WriteLine(City, Description);
+        var site = await _context.Sites.FindAsync(Id);
+        if (site == null)
+        {
+            return NotFound();
+        }
+
+        site.City = City;
+        site.Description = Description;
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int Id)
+    {
+        var site = await _context.Sites.FindAsync(Id);
+        if (site == null)
+        {
+            return NotFound();
+        }
+
+        _context.Sites.Remove(site);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(int Id)
+    {
+        var site = await _context.Sites
+            .Include(s => s.Employees)
+            .ThenInclude(e => e.Site)
+            .FirstOrDefaultAsync(s => s.SiteId == Id);
+        if (site == null)
+        {
+            return NotFound();
+        }
+
+        return View(site);
     }
 }
