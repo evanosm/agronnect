0836e127acbd1f8a89bb112a3aa3a088e191f50a
Evan OSMONT <evan.osmont@gmail.com>
2023-04-27 11:28:07 +0200

    Nullable Site Desc

--- Models/SiteModel.cs
+++ Models/SiteModel.cs
@@ -6,6 +6,6 @@ public class Site
 {
     [Key] public int SiteId { get; set; }
     public string City { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public ICollection<Employee>? Employees { get; set; }
 }
